3253e46b5fd1a70365ca2b60065d8336ea0c8b9a
bananazhao <877252992@qq.com>
2018-07-16 10:50:00 +0800

    自定义过滤器测试

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,12 +1,24 @@
 package com.example.demo.controller;
 
+import com.example.demo.domain.User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author JIAOJIAO
+ */
 @RestController
 public class HelloController {
     @RequestMapping("/hello")
     public String index() {
         return "HelloWorld";
+    }
+
+    @RequestMapping("getUser")
+    public User getUser() {
+        User user=new User();
+        user.setUserName("小明");
+        user.setPassWord("xxxx");
+        return user;
     }
 }
---
+++ src/main/java/com/example/demo/domain/User.java
@@ -0,0 +1,23 @@
+package com.example.demo.domain;
+
+public class User {
+
+    private String UserName;
+    private String PassWord;
+
+    public String getUserName() {
+        return UserName;
+    }
+
+    public void setUserName(String userName) {
+        UserName = userName;
+    }
+
+    public String getPassWord() {
+        return PassWord;
+    }
+
+    public void setPassWord(String passWord) {
+        PassWord = passWord;
+    }
+}
