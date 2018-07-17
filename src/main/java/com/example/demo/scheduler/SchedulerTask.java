643c4884f9f4b52638053801028f7d89f4d76ae3
bananazhao <877252992@qq.com>
2018-07-17 14:23:52 +0800

    定时任务测试

---
+++ src/main/java/com/example/demo/scheduler/SchedulerTask.java
@@ -0,0 +1,19 @@
+package com.example.demo.scheduler;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author JIAOJIAO
+ */
+@Component
+public class SchedulerTask {
+
+    private int count = 0;
+
+    @Scheduled(cron="*/6 * * * * ?")
+    private void process(){
+        System.out.println("this is scheduler task runing  "+(count++));
+    }
+
+}
